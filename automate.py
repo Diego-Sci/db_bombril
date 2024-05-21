920bde255095e7d0163e362df9f64952c8cbf125
Diego-Sci <diego.santos@bombril.com.br>
2024-05-21 11:45:29 -0300

    Create automate.py
    criação da automação

---
+++ automate.py
@@ -0,0 +1,25 @@
+#proceso de automação
+
+import pyautogui as pd
+import time
+
+def tempo():
+  time.sleep(2)
+
+pd.press("win")
+time.sleep(2)
+
+pd.write("chrome")
+time.sleep(2)
+
+
+pd.press("enter")
+time.sleep(2)
+
+pd.write("https://sites.google.com/bombril.com.br/portalmelhoriacontinua/p%C3%A1gina-inicial")
+time.sleep(2)
+
+pd.press("enter")
+
+
+  
